907590eea53a530091c43a19d60b4bd2ec46a44e
Piotr Tarsa <piotr.tarsa@gmail.com>
2015-11-13 19:02:14 +0100

    Created module 'common'

--- project/MainBuild.scala
+++ project/MainBuild.scala
@@ -1,5 +1,25 @@
+/*
+ * Copyright (C) 2015 Piotr Tarsa ( http://github.com/tarsa )
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the author be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
 import sbt._
-import Keys._
+import sbt.Keys._
 
 object MainBuild extends Build {
 
@@ -8,6 +28,7 @@ object MainBuild extends Build {
   lazy val commonSettings = Seq(
     scalaVersion := theScalaVersion,
     conflictManager := ConflictManager.strict,
+    scalacOptions += "-feature",
     dependencyOverrides ++= Set(
       "org.scala-lang" % "scala-library" % theScalaVersion,
       "org.scala-lang" % "scala-reflect" % theScalaVersion,
@@ -27,9 +48,7 @@ object MainBuild extends Build {
         "org.scalafx" %% "scalafx" % "8.0.40-R8")
     )
 
-  lazy val depsCp = deps % fullDep
-
-  lazy val rootDeps = Seq(core, fxgui, natives, opencl, random, sorts)
+  lazy val rootDeps = Seq(common, core, fxgui, natives, opencl, random, sorts)
 
   lazy val root = Project(id = "SortingAlgorithmsToolbox", base = file("."))
     .settings(
@@ -39,31 +58,37 @@ object MainBuild extends Build {
     .aggregate(rootDeps.map(Project.projectToRef): _*)
     .dependsOn(rootDeps.map(p => classpathDependency(p)): _*)
     .settings(rootDeps.map(dep => discoveredMainClasses in Compile <++=
-    discoveredMainClasses in dep in Compile): _*)
+      discoveredMainClasses in dep in Compile): _*)
+
+  lazy val common = Project(id = "common", base = file("./common"))
+    .settings(commonSettings: _*)
+    .dependsOn(deps % fullDep)
+
+  lazy val commonCp = common % fullDep
 
   lazy val core = Project(id = "core", base = file("./core"))
     .settings(commonSettings: _*)
-    .dependsOn(depsCp, nativesCp, opencl, random)
+    .dependsOn(commonCp, nativesCp, opencl, random)
 
   lazy val fxgui = Project(id = "fxgui", base = file("./fxgui"))
     .settings(commonSettings: _*)
-    .dependsOn(depsCp, core)
+    .dependsOn(commonCp, core)
 
   lazy val natives = Project(id = "natives", base = file("./natives"))
     .settings(commonSettings: _*)
-    .dependsOn(depsCp)
+    .dependsOn(commonCp)
 
   lazy val nativesCp = natives % fullDep
 
   lazy val opencl = Project(id = "opencl", base = file("./opencl"))
     .settings(commonSettings: _*)
-    .dependsOn(depsCp)
+    .dependsOn(commonCp)
 
   lazy val random = Project(id = "random", base = file("./random"))
     .settings(commonSettings: _*)
-    .dependsOn(depsCp, nativesCp, opencl)
+    .dependsOn(commonCp, nativesCp, opencl)
 
   lazy val sorts = Project(id = "sorts", base = file("./sorts"))
     .settings(commonSettings: _*)
-    .dependsOn(depsCp, core, nativesCp)
+    .dependsOn(commonCp, core, nativesCp)
 }
