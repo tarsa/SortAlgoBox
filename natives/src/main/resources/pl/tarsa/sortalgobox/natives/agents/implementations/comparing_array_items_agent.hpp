58f42e69ded24a5a4a21f278a6c5840b3a314b51
Piotr Tarsa <piotr.tarsa@gmail.com>
2016-06-21 01:08:10 +0200

    Made native C++ items agents

---
+++ natives/src/main/resources/pl/tarsa/sortalgobox/natives/agents/implementations/comparing_array_items_agent.hpp
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2015, 2016 Piotr Tarsa ( http://github.com/tarsa )
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the author be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+#ifndef COMPARING_ARRAY_ITEMS_AGENT_HPP
+#define COMPARING_ARRAY_ITEMS_AGENT_HPP
+
+#include <cstdint>
+#include <cstring>
+
+#include "comparing_items_agent.hpp"
+
+namespace tarsa {
+
+    template<typename ItemType>
+    class ComparingArrayItemsAgent : public ComparingItemsAgent<ItemType> {
+        ItemType * const array;
+        size_t const count;
+
+    public:
+        ComparingArrayItemsAgent(ItemType * const array, size_t const count):
+            array(array), count(count) {
+        }
+
+        size_t size0() const {
+            return count;
+        }
+
+        ItemType get0(size_t const i) const {
+            return array[i];
+        }
+
+        void set0(size_t const i, ItemType const v) {
+            array[i] = v;
+        }
+
+        void copy0(size_t const i, size_t const j, size_t const n) {
+            memcpy(array + j, array + i, n + sizeof(ItemType));
+        }
+
+        void swap0(size_t const i, size_t const j) {
+            ItemType const temp = get0(i);
+            set0(i, get0(j));
+            set0(j, temp);
+        }
+
+        compare_t compare(ItemType const a, ItemType const b) const {
+            if (a < b) {
+                return CompareBelow;
+            } else if (a == b) {
+                return CompareEqual;
+            } else {
+                return CompareAbove;
+            }
+        }
+
+        bool compareLt(ItemType const a, ItemType const b) const {
+            return a < b;
+        }
+
+        compare_t compare0(size_t const i, size_t const j) const {
+            return compare(get0(i), get0(j));
+        }
+
+        bool compareLt0(size_t const i, size_t const j) const {
+            return compareLt(get0(i), get0(j));
+        }
+    };
+}
+
+#endif /* COMPARING_ARRAY_ITEMS_AGENT_HPP */
---
+++ natives/src/main/resources/pl/tarsa/sortalgobox/natives/agents/implementations/recording_comparing_items_agent.hpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2015, 2016 Piotr Tarsa ( http://github.com/tarsa )
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the author be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+#ifndef RECORDING_COMPARING_ITEMS_AGENT_HPP
+#define RECORDING_COMPARING_ITEMS_AGENT_HPP
+
+#include <cstdint>
+#include <cstring>
+
+#include "action_codes.hpp"
+#include "comparing_items_agent.hpp"
+
+namespace tarsa {
+
+    template<template<typename> class UnderlyingItemsAgent, typename ItemType>
+    class RecordingComparingItemsAgent : public ComparingItemsAgent<ItemType> {
+        NumberEncoder recorder;
+        UnderlyingItemsAgent<ItemType> underlying;
+
+    public:
+        RecordingComparingItemsAgent(NumberEncoder const recorder,
+                UnderlyingItemsAgent<ItemType> const underlying):
+            recorder(recorder), underlying(underlying) {
+        }
+
+        size_t size0() {
+            recorder.serializeInt(CodeSize0);
+            return underlying.size0();
+        }
+
+        ItemType get0(size_t const i) {
+            recorder.serializeInt(CodeGet0);
+            recorder.serializeLong(i);
+            return underlying.get0(i);
+        }
+
+        void set0(size_t const i, ItemType const v) {
+            recorder.serializeInt(CodeSet0);
+            recorder.serializeLong(i);
+            underlying.set0(i, v);
+        }
+
+        void copy0(size_t const i, size_t const j, size_t const n) {
+            recorder.serializeInt(CodeCopy0);
+            recorder.serializeLong(i);
+            recorder.serializeLong(j);
+            recorder.serializeLong(n);
+            underlying.copy0(i, j, n);
+        }
+
+        void swap0(size_t const i, size_t const j) {
+            recorder.serializeInt(CodeSwap0);
+            recorder.serializeLong(i);
+            recorder.serializeLong(j);
+            underlying.swap0(i, j);
+        }
+
+        compare_t compare(ItemType const a, ItemType const b) {
+            recorder.serializeInt(CodeCompare);
+            return underlying.compare(a, b);
+        }
+
+        bool compareLt(ItemType const a, ItemType const b) {
+            recorder.serializeInt(CodeCompare);
+            return underlying.compareLt(a, b);
+        }
+
+        compare_t compare0(size_t const i, size_t const j) {
+            recorder.serializeInt(CodeCompare0);
+            recorder.serializeLong(i);
+            recorder.serializeLong(j);
+            return underlying.compare0(i, j);
+        }
+
+        bool compareLt0(size_t const i, size_t const j) {
+            recorder.serializeInt(CodeCompare0);
+            recorder.serializeLong(i);
+            recorder.serializeLong(j);
+            return underlying.compareLt0(i, j);
+        }
+    };
+}
+
+#endif /* RECORDING_COMPARING_ITEMS_AGENT_HPP */
