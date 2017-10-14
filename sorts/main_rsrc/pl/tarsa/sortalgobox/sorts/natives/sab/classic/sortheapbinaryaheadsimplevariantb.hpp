8c82e67f06b20bda82d1698c4bd729bd2645d704
Piotr Tarsa <piotr.tarsa@gmail.com>
2017-10-15 00:30:47 +0200

    Refactored classic SAB sortheapbinaryaheadsimplevariantb.hpp

--- sorts/main_rsrc/pl/tarsa/sortalgobox/sorts/natives/sab/classic/sortheapbinaryaheadsimplevariantb.hpp
+++ sorts/main_rsrc/pl/tarsa/sortalgobox/sorts/natives/sab/classic/sortheapbinaryaheadsimplevariantb.hpp
@@ -1,7 +1,7 @@
 /* 
  * sortheapbinaryaheadsimplevariantb.hpp -- sorting algorithms benchmark
  * 
- * Copyright (C) 2014 Piotr Tarsa ( http://github.com/tarsa )
+ * Copyright (C) 2014 - 2017 Piotr Tarsa ( http://github.com/tarsa )
  *
  *  This software is provided 'as-is', without any express or implied
  *  warranty.  In no event will the author be held liable for any damages
@@ -18,7 +18,6 @@
  *  2. Altered source versions must be plainly marked as such, and must not be
  *     misrepresented as being the original software.
  *  3. This notice may not be removed or altered from any source distribution.
- * 
  */
 #ifndef SORTHEAPBINARYAHEADSIMPLEVARIANTB_HPP
 #define	SORTHEAPBINARYAHEADSIMPLEVARIANTB_HPP
@@ -27,11 +26,12 @@
 
 namespace tarsa {
 
-    namespace privateBinaryHeapSortAheadSimpleVariantB {
+    template<typename ItemType, ComparisonOperator<ItemType> compOp>
+    class TheSorter {
+        ItemType * const a;
+        ssize_t const count;
 
-        template<typename ItemType, ComparisonOperator<ItemType> compOp>
-        void siftDown(ItemType * const a, ssize_t const start,
-                ssize_t const end) {
+        void siftDown(ssize_t const start, ssize_t const end) {
             ssize_t index = start;
             while (index * 2 + 1 <= end) {
                 ssize_t const left = index * 2;
@@ -53,33 +53,34 @@ namespace tarsa {
             a[start] = tmp;
         }
 
-        template<typename ItemType, ComparisonOperator<ItemType> compOp>
-        void heapify(ItemType * const a, ssize_t const count) {
+        void heapify() {
             for (ssize_t item = count / 2; item >= 1; item--) {
-                siftDown<ItemType, compOp>(a, item, count);
+                siftDown(item, count);
             }
         }
 
-        template<typename ItemType, ComparisonOperator<ItemType> compOp>
-        void drainHeap(ItemType * const a, ssize_t const count) {
+        void drainHeap() {
             for (ssize_t next = count; next > 1; next--) {
                 std::swap(a[next], a[1]);
-                siftDown<ItemType, compOp>(a, 1, next - 1);
+                siftDown(1, next - 1);
             }
         }
 
-        template<typename ItemType, ComparisonOperator<ItemType> compOp>
-        void heapsort(ItemType * const a, ssize_t const count) {
-            heapify<ItemType, compOp>(a, count);
-            drainHeap<ItemType, compOp>(a, count);
+    public:
+        TheSorter(ItemType * const a, ssize_t const count):
+            a(a), count(count) {
         }
-    }
+
+        void heapsort() {
+            heapify();
+            drainHeap();
+        }
+    };
 
     template<typename ItemType, ComparisonOperator<ItemType> compOp>
     void BinaryHeapSortAheadSimpleVariantB(ItemType * const a,
             ssize_t const count) {
-        privateBinaryHeapSortAheadSimpleVariantB::heapsort<ItemType, compOp>(
-                a - 1, count);
+        TheSorter<ItemType, compOp>(a - 1, count).heapsort();
     }
 
     template<typename ItemType>
